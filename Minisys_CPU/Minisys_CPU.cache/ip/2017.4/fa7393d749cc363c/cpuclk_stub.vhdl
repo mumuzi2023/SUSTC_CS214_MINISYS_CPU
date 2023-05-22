a66d8021d5cc6e579f2621e99ff552c74a3b551a
404wrong <2497440459@qq.com>
2023-05-22 19:32:23 +0800

    add clk_ip_core

---
+++ Minisys_CPU/Minisys_CPU.cache/ip/2017.4/fa7393d749cc363c/cpuclk_stub.vhdl
@@ -0,0 +1,30 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Mon May 22 19:31:13 2023
+-- Host        : Gao running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ cpuclk_stub.vhdl
+-- Design      : cpuclk
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tfgg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    single_cycle_cpu_clk : out STD_LOGIC;
+    uart_clk : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "single_cycle_cpu_clk,uart_clk,clk_in1";
+begin
+end;
