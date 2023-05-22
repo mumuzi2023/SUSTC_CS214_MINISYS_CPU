c4d73fd99bc7e7ead81f54d15f6937b3b22f1255
404wrong <2497440459@qq.com>
2023-05-22 17:06:26 +0800

    Add programrom
    more need to change:
    uart and ifetc

---
+++ Minisys_CPU/Minisys_CPU.srcs/sources_1/new/program_rom.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/22 16:52:17
+// Design Name: 
+// Module Name: program_rom
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module program_rom(
+
+    );
+endmodule
