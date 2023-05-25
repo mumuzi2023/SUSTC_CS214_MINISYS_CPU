f5438bed7f6930901c8104178cb68a6a925cdc31
404wrong <2497440459@qq.com>
2023-05-25 15:19:51 +0800

    update pad

--- Minisys_CPU/Minisys_CPU.srcs/sources_1/new/keypad_n.v
+++ Minisys_CPU/Minisys_CPU.srcs/sources_1/new/keypad_n.v
@@ -40,17 +40,34 @@ always@(posedge clk_10[7:7])begin
     else begin line<=4'b1110;now<=1;end
 end
 always@(posedge clk_10[2:2])begin
-    if(now==1&&!row[0:0]) v1<=1;
-    else if(now==2&&!row[0:0]) v1<=2;
-    else if(now==3&&!row[0:0]) v1<=3;
-    else if(now==1&&!row[1:1]) v1<=4;
-    else if(now==2&&!row[1:1]) v1<=5;
-    else if(now==3&&!row[1:1]) v1<=6;
-    else if(now==1&&!row[2:2]) v1<=7;
-    else if(now==2&&!row[2:2]) v1<=8;
-    else if(now==3&&!row[2:2]) v1<=9;
-    else if(now==2&&!row[3:3]) v1<=0;
-    else if(v1-now==0||v1-now==3||v1-now==6||v1-now==9) v1<=10;
+    if(now==1)begin
+        if(!(row[0:0]*row[1:1]*row[2:2]*row[3:3]))begin
+            if(!row[0:0]) v1<=1;
+            else if(!row[1:1]) v1<=4;
+            else if(!row[2:2]) v1<=7;
+            else if(!row[3:3]) v1<=14;
+        end
+        else if(v1==1||v1==4||v1==7||v1==14) v1<=15;
+    end
+    else if(now==2)begin
+        if(!(row[0:0]*row[1:1]*row[2:2]*row[3:3]))begin
+            if(!row[0:0]) v1<=2;
+            else if(!row[1:1]) v1<=5;
+            else if(!row[2:2]) v1<=8;
+            else if(!row[3:3]) v1<=0;
+        end
+        else if(v1==2||v1==5||v1==8||v1==0) v1<=15;
+    end
+    else if(now==3)begin
+        if(!(row[0:0]*row[1:1]*row[2:2]*row[3:3]))begin
+            if(!row[0:0]) v1<=3;
+            else if(!row[1:1]) v1<=6;
+            else if(!row[2:2]) v1<=9;
+            else if(!row[3:3]) v1<=15;
+        end
+        else if(v1==3||v1==6||v1==9||v1==15) v1<=15;
+    end
+
 end
 
 endmodule
--- Minisys_CPU/Minisys_CPU.srcs/sources_1/new/pad_decode.v
+++ Minisys_CPU/Minisys_CPU.srcs/sources_1/new/pad_decode.v
@@ -19,21 +19,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module pad_decode(clk,in,o1,o2,o3,o4);
+//* is reset a
+module pad_decode(clk,in,o1,o2,o3,o4,rst);
 input clk;
+input rst;
 input[3:0]in;
 output reg[3:0]o1,o2,o3,o4;
-reg [8:0] cnt=0;
+reg [9:0] cnt=0;
 reg [3:0] last;
 reg b;
 
 always@(posedge clk) begin
-    if(last!=in||last==10) begin cnt<=0;last<=in;b<=1;end
+    if(rst)begin o1<=0;o2<=0;o3<=0;o4<=0;cnt<=0;last<=15;b<=1;end
+    else if(last!=in||last==15) begin
+        if(cnt!=0)cnt<=cnt-1;
+        else begin cnt<=0;last<=in;b<=1;end
+    end
     else begin
         if(b==1)begin
-            if(!cnt[8:8])cnt<=cnt+1;
-            else begin b=0;o4<=o3;o3<=o2;o2<=o1;o1<=last;end
+            if(!cnt[9:9])cnt<=cnt+1;
+            else begin
+                if(last==14)begin b=0;o4<=0;o3<=0;o2<=0;o1<=0;end
+                else begin b=0;o4<=o3;o3<=o2;o2<=o1;o1<=last; end
+            end
         end
         
     end
