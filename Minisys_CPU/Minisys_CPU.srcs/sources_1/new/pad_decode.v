36d9eff0aef28ec7d080623ff5432a91906b1c9a
404wrong <2497440459@qq.com>
2023-05-26 11:18:48 +0800

    test

--- Minisys_CPU/Minisys_CPU.srcs/sources_1/new/pad_decode.v
+++ Minisys_CPU/Minisys_CPU.srcs/sources_1/new/pad_decode.v
@@ -20,17 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 //* is reset a
-module pad_decode(clk,in,o1,o2,o3,o4,rst);
-input clk;
+module pad_decode(clk,in,o1,o2,o3,o4,o5,o6,o7,o8,rst,o_4);
+input clk;//      时钟，输入，输出（单个数字），重置，输出【o1,o2,o3,o4]组合
 input rst;
 input[3:0]in;
-output reg[3:0]o1,o2,o3,o4;
+output [15:0]o_4;
+output reg[3:0]o1,o2,o3,o4,o5,o6,o7,o8;
 reg [9:0] cnt=0;
 reg [3:0] last;
 reg b;
-
+assign o_4=o4*1000+o3*100+o2*10+o1;
 always@(posedge clk) begin
-    if(rst)begin o1<=0;o2<=0;o3<=0;o4<=0;cnt<=0;last<=15;b<=1;end
+    if(rst)begin o1<=0;o2<=0;o3<=0;o4<=0;o5<=0;o6<=0;o7<=0;o8<=0;cnt<=0;last<=15;b<=1;end
     else if(last!=in||last==15) begin
         if(cnt!=0)cnt<=cnt-1;
         else begin cnt<=0;last<=in;b<=1;end
@@ -39,8 +40,8 @@ always@(posedge clk) begin
         if(b==1)begin
             if(!cnt[9:9])cnt<=cnt+1;
             else begin
-                if(last==14)begin b=0;o4<=0;o3<=0;o2<=0;o1<=0;end
-                else begin b=0;o4<=o3;o3<=o2;o2<=o1;o1<=last; end
+                if(last==14)begin b=0;o8<=0;o7<=0;o6<=0;o5<=0;o4<=0;o3<=0;o2<=0;o1<=0;end
+                else begin b=0;o8<=o7;o7<=o6;o6<=o5;o5<=o4;o4<=o3;o3<=o2;o2<=o1;o1<=last; end
             end
         end
         
